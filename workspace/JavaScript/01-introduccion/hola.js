213514c5a95c88a5ffbeaa6e0937d242f5e9bc16
JuanE-Gil <96081036+JuanE-Gil@users.noreply.github.com>
2022-12-01 12:33:01 -0500

    a

---
+++ workspace/JavaScript/01-introduccion/hola.js
@@ -0,0 +1,9 @@
+
+/* Imprimiendo la cadena 'Hola Mundo' en la consola. */
+console.log('Hola Mundo');
+
+/* Escribiendo la cadena '<h1> hola mundo </h1>'  al documento. */
+document.write('<h1> Hola Mundo </h1>');
+
+/* Mostrando una ventana emergente con el texto 'Hola Mundo'. */
+alert('Hola Mundo');
---
+++ workspace/JavaScript/01-introduccion/variables.js
@@ -0,0 +1,14 @@
+//Definición de Variables
+
+/* Definiendo una variable llamada `nombre` y asignándole el valor `Juan`. */
+
+let nombre = 'Juan';
+console.log(nombre);
+
+// Forma Antigua
+// var nombre = 'Juan';
+// console.log(nombre);
+
+// Modificar valor de la variable
+nombre = 25;
+console.log(nombre);
